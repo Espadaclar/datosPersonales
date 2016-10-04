329bc8afeb3bb240a389e73ef289517eb858439c
Espadaclar <eneroborde8002@gmail.com>
2016-10-05 00:02:49 +0200

    Añadido código para mostra la localidad.

--- DatosP.java
+++ DatosP.java
@@ -3,5 +3,6 @@ public class DatosP{
 	public static void main(String args[]){
 		System.out.println("franciscojavier");
 		System.out.println("edad 51 años.");
+		System.out.println("localidad  León.");
 	}
 }
